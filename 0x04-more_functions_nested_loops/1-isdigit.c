cbac2529fe979972b4c88f5b64f515a7acdb1fd0
Zarakalil <officialzarakalil@gmail.com>
2023-02-23 15:42:53 -0800

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isdigit - Check for a digit
+ * @c: The number to be checked
+ * Retunr: 1 for digit character or 0 for others
+ */
+
+int _isdigit(int x)
+
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
