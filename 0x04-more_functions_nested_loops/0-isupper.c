defbce1ccc58e55f62a1c781aefed17eba565b0d
Zarakalil <officialzarakalil@gmail.com>
2023-02-23 14:48:48 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper -  Check for upper letter
+ * @x: Number to check
+ * Return: 1 for upper or 0 for others
+ */
+
+int _isuper(int x)
+{
+	if (x>= 65 && x <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
