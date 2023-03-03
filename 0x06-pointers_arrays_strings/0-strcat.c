825df155508822f164ad42539da547b5feac37f8
Zarakalil <officialzarakalil@gmail.com>
2023-03-03 00:09:42 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,14 +7,13 @@
  * Return: 0
  */
 int main(void)
-
-Char *_strcat(char *dest, char *src)
 {
+Char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
 	i = 0;
-	while (dest[i] != '\0')
+	whilei (dest[i] != '\0')
 	{
 		i++;
 	}
